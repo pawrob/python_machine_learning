acb2852b5dce39d8ed78cd14565bfb0ff8d3f70f
Paweł Bucki <mrpawrob@gmail.com>
2021-02-18 16:59:30 +0100

    Added [knn predictions]

--- src/knn.py
+++ src/knn.py
@@ -1,12 +1,9 @@
-import sklearn
 import pandas as pd
-import numpy as np
-from sklearn.utils import shuffle
+import sklearn
+from sklearn import preprocessing
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn import linear_model, preprocessing
 
 data = pd.read_csv("../datasets/car.data")
-print(data.head)
 
 le = preprocessing.LabelEncoder()
 buying = le.fit_transform(list(data["buying"]))
@@ -18,8 +15,25 @@ safety = le.fit_transform(list(data["safety"]))
 cls = le.fit_transform(list(data["class"]))
 
 predict = "class"
-
-X = list(zip(buying,maint,doors,persons,lug_boot,safety,cls))
+names = ["unacc", "acc", "good", "vgood"]
+accuracy = 0
+X = list(zip(buying, maint, doors, persons, lug_boot, safety))
 y = list(cls)
+model = KNeighborsClassifier(n_neighbors=7)
 
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.1)
+
+model.fit(x_train, y_train)
+acc = model.score(x_test, y_test)
+print(acc)
+predicted = model.predict(x_test)
+
+for x in range(len(predicted)):
+    print("Predicted: ", names[predicted[x]], "\tData: ", x_test[x], "Actual: ", names[y_test[x]])
+    if predicted[x] == y_test[x]:
+        accuracy += 1
+
+accuracy = accuracy / len(predicted)
+
+print("Accuracy based on sklearn: ", acc)
+print("Accuracy based on result:  ", accuracy)
