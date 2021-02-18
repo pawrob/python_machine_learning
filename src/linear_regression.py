a01051e2371c859118c4988de163ae060657d2dc
Paweł Bucki <mrpawrob@gmail.com>
2021-02-18 15:51:56 +0100

    Update linear_regression.py

--- src/linear_regression.py
+++ src/linear_regression.py
@@ -1,51 +1,46 @@
-import os
-
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-import tensorflow as tf
-import keras
 import numpy as np
 import pandas as pd
 import sklearn
-from sklearn import linear_model
-from sklearn.utils import shuffle
 import matplotlib.pyplot as pyplot
 import pickle
+from sklearn import linear_model
 from matplotlib import style
+
 data = pd.read_csv("student-mat.csv", sep=";")
-
 data = data[["G1", "G2", "G3", "studytime", "failures", "absences"]]
-
 predict = "G3"
 best_acc = 0
 X = np.array(data.drop([predict], 1))
 y = np.array(data[predict])
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.1)
 
+# training model and selecting one with best accuracy
 for _ in range(50):
-    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.1)
     linear = linear_model.LinearRegression()
     linear.fit(x_train, y_train)
     acc = linear.score(x_test, y_test)
 
-    if acc>best_acc:
+    if acc > best_acc:
         best_acc = acc
         with open("studentmodel.pickle", "wb") as f:
             pickle.dump(linear, f)
 
-pickle_in = open("studentmodel.pickle", "rb" )
+pickle_in = open("studentmodel.pickle", "rb")
 linear = pickle.load(pickle_in)
 
 print("Co: ", linear.coef_)
 print("Int: ", linear.intercept_)
 
+# predicting
 predictions = linear.predict(x_test)
-print("Acc: ", acc)
+print("Acc: ", best_acc)
 for x in range(len(predictions)):
     print(predictions[x], x_test[x], y_test[x])
 
+# plotting
 p = "G1"
 style.use("ggplot")
-pyplot.scatter(data[p],data["G3"])
+pyplot.scatter(data[p], data["G3"])
 pyplot.xlabel(p)
 pyplot.ylabel("Final grade")
 pyplot.show()
